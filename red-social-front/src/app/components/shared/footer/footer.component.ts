674f0a2f0928fdc096bf6f275b1495adb108631d
lucag316 <lucafranco316@gmail.com>
2025-06-03 20:01:16 -0300

    Haciendo footer

--- red-social-front/src/app/components/shared/footer/footer.component.ts
+++ red-social-front/src/app/components/shared/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
+
+
 export class FooterComponent {
 
+  constructor(private router: Router){}
 }
