cb04dc0e7ebaa3f9fefc821e5781a8faf49a9c85
lucag316 <lucafranco316@gmail.com>
2025-06-04 11:41:45 -0300

    Mejorando registro

--- red-social-front/src/app/components/pages/registro/registro.component.ts
+++ red-social-front/src/app/components/pages/registro/registro.component.ts
@@ -6,19 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { AuthService } from '../../../services/auth/auth.service';
 
-
-/*
-
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { Router } from '@angular/router';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import import { CommonModule from '@angular/common';
-
-import 
-*/
-
 @Component({
     standalone: true,
     selector: 'app-registro',
